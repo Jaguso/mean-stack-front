6972e091f72d9859341061a972a3dc73ff49f666
Jaguso <jagutzo@gmail.com>
2019-03-04 19:34:34 -0600

    added method to GET all issues

--- src/app/issue.service.ts
+++ src/app/issue.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssueService {
 
-  constructor() { }
+  uri = 'http://localhost:4000';
+
+  constructor(private http: HttpClient) { }
+
+  getIssues() {
+    return this.http.get(`${this.uri}/issues`);
+  }
+
 }
