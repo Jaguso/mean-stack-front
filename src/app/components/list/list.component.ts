b0c8eb49a4a6a922cb348a0ccf033daeb145d978
Jaguso <jagutzo@gmail.com>
2019-03-04 20:58:31 -0600

    injected editIssue and deleteIssue services

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -37,4 +37,14 @@ export class ListComponent implements OnInit {
       });
   }
 
+  editIssue(id) {
+    this.router.navigate([`/edit/${id}`]);
+  }
+
+  deleteIssue(id) {
+    this.issueService.deleteIssue(id).subscribe(() => {
+      this.fetchIssues();
+    });
+  }
+
 }
