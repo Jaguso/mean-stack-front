830fb9bedf72434a30f81cb044b835c4992df282
Jaguso <jagutzo@gmail.com>
2019-03-04 20:13:17 -0600

    checked if the getIssues() method works

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -11,6 +11,10 @@ export class ListComponent implements OnInit {
   constructor(private issueService: IssueService) { }
 
   ngOnInit() {
+    // next lines are to check the service is working
+    this.issueService.getIssues().subscribe((issues) => {
+      console.log(issues);
+    });
   }
 
 }
