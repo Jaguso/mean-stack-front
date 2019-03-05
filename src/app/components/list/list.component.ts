49138a195094fbe5cc3cffce2266b3ab3f9a6e5d
Jaguso <jagutzo@gmail.com>
2019-03-04 20:37:36 -0600

    implemented getIssues method from service

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
+
+import { Issue } from '../../issue.model';
 import { IssueService } from '../../issue.service';
 
 @Component({
@@ -8,13 +12,29 @@ import { IssueService } from '../../issue.service';
 })
 export class ListComponent implements OnInit {
 
-  constructor(private issueService: IssueService) { }
+  issues: Issue[];
+  displayedColumns = [
+    'title', 
+    'responsible',
+    'severity',
+    'status',
+    'actions'
+  ];
+
+  constructor(private issueService: IssueService, private router: Router) { }
 
   ngOnInit() {
-    // next lines are to check the service is working
-    this.issueService.getIssues().subscribe((issues) => {
-      console.log(issues);
-    });
+    this.fetchIssues();
+  }
+
+  fetchIssues() {
+    this.issueService
+      .getIssues()
+      .subscribe((data: Issue[]) => {
+        this.issues = data;
+        console.log('Data requested ...');
+        console.log(this.issues);
+      });
   }
 
 }
